205c8b2c0f1eb7a2a084aeeaac42cf5a8b72add8
Ajeet <pajitive@gmail.com>
2020-03-26 00:04:01 +0530

    hashing password

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -15,15 +15,16 @@ module.exports = {
           password,
           phone
         });
-        let resultData = await jwt.sign({ userData: User }, "secret", {
-          expiresIn: 60 * 60
-        });
-        if (resultData) {
+        // let resultData = await jwt.sign({ userData: User }, "secret", {
+        //   expiresIn: 60 * 60
+        // });
+        // if (resultData) {
           let salt = await bcrypt.getSalt(10);
           User.password = await bcrypt.hash(password,salt);
           let newUser = await User.save();
-          res.status(200).send({ result: true, token: resultData });
-        }
+          res.status(200).send({ result: true, message:'User is registered successfully...!' });
+          // res.status(200).send({ result: true, token: resultData });
+        // }
       }
     } catch (e) {
       console.log(e);
@@ -35,9 +36,14 @@ module.exports = {
     try {
     let result = await user.findOne({email:req.body.email});
     if(result ==undefined || !result)
-     res.status(208).send({result:false,message:'User is already registered!'});
+     res.status(208).send({result:false,message:'Invalid User Name...!'});
      else{
-
+      let isValidPassword = await bcrypt.compare(req.body.password,result.password);
+      if(!isValidPassword)
+       res.status(400).send({result:false,message:'Invalid Password...!'});
+       else{
+           res.status(200).send({result:true});
+       }
      }
     } 
     catch (e) {
